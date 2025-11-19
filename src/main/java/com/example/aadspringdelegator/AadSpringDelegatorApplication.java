a65544c37b12dd8b397df61f7a5db88e4e78dd5d
Niels Van Zandbergen <niels.vanzandbergen@cegeka.com>
2025-11-19 16:53:01 +0100

    Spring Starter Added

---
+++ src/main/java/com/example/aadspringdelegator/AadSpringDelegatorApplication.java
@@ -0,0 +1,13 @@
+package com.example.aadspringdelegator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AadSpringDelegatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AadSpringDelegatorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/aadspringdelegator/AadSpringDelegatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.aadspringdelegator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AadSpringDelegatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
